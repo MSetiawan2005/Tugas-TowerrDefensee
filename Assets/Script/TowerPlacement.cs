8d1f4b3b99c7e0ab7b12cadf01e1474b6da6da73
MSetiawan2005 <ms0132005@gmail.com>
2021-09-19 08:44:55 +0700

    upload-tugasTowerDef

---
+++ Assets/Script/TowerPlacement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class TowerPlacement : MonoBehaviour
+{
+    private Tower _placedTower;
+
+    // Fungsi yang terpanggil sekali ketika ada object Rigidbody yang menyentuh area collider
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (_placedTower != null)
+        {
+            return;
+        }
+
+        Tower tower = collision.GetComponent<Tower>();
+        if (tower != null)
+        {
+            tower.SetPlacePosition(transform.position);
+            _placedTower = tower;
+        }
+    }
+
+    // Kebalikan dari OnTriggerEnter2D, fungsi ini terpanggil sekali ketika object tersebut meninggalkan area collider
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (_placedTower == null)
+        {
+            return;
+        }
+
+        _placedTower.SetPlacePosition(null);
+        _placedTower = null;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Tugas-TowerrDefensee
- Tugas Tower Defense - AGATE CH4 (Muhammad Setiawan)
+# Agate - Tower Defense
+
+Tugas Tower Defense Agate ch 4, Pertahanan menara merupakan subgenre dari permainan video strategi di mana tujuannya adalah untuk mempertahankan wilayah atau barang milik pemain dari serangan musuh, yang biasanya dicapai dengan menempatkan struktur pertahanan di sepanjang jalur serangan mereka.
+
+## Peserta Studi Independent
+
+Muhammad Setiawan | ID AGATE : 437
+
+## Reminder
+Don't forget to ⭐️ this project
+
+## How To Run
+
+
+## Features & Keybinds
+
+- This is a 1 player game
+- Controls:
+  - Left click 
+  
+## Additional Features
+- Backsound relax music
+- Sfx sounds 
+
+## Updates
+
+- 
